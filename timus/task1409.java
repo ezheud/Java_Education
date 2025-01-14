1570b3f5ac65bbca6f1012abcd0337b0f153c371
Sergey <valerijminkajdarov9@gmail.com>
2025-01-14 17:31:34 +0500

    timus task

---
+++ timus/task1409.java
@@ -0,0 +1,26 @@
+package timus;
+
+import java.util.Scanner;
+
+public class task1409 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ввод количества банок, простреленных Гарри и Ларри
+        System.out.print("Введите количество банок, простреленных Гарри и Ларри через пробел: ");
+        int G = scanner.nextInt();
+        int L = scanner.nextInt();
+
+        // Общее количество банок
+        int N = G + L - 1;
+
+        // Количество банок, не простреленных Гарри и Ларри
+        int unshot_G = N - G;
+        int unshot_L = N - L;
+
+        // Вывод результата
+        System.out.println(unshot_G + " " + unshot_L);
+
+        scanner.close();
+    }
+}
---
+++ timus/task1787.java
@@ -0,0 +1,56 @@
+package timus;
+
+import java.util.Scanner;
+
+public class task1787 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Проверка, были ли переданы аргументы командной строки
+        if (args.length > 0) {
+            // Если аргументы переданы, парсим их
+            int k = Integer.parseInt(args[0]);
+            int n = Integer.parseInt(args[1]);
+            int[] cars = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                cars[i] = Integer.parseInt(args[i + 2]);
+            }
+
+            // Вычисление количества машин в пробке
+            int jamCount = calculateJamCount(k, n, cars);
+            System.out.println(jamCount);
+        } else {
+            // Если аргументы не переданы, запрашиваем ввод у пользователя
+            System.out.println("Введите количество машин, которые могут повернуть за минуту и количество минут наблюдения:");
+            int k = scanner.nextInt();
+            int n = scanner.nextInt();
+
+            int[] cars = new int[n];
+            System.out.println("Введите количество машин, подъехавших к повороту за каждую минуту:");
+            for (int i = 0; i < n; i++) {
+                cars[i] = scanner.nextInt();
+            }
+
+            // Вычисление количества машин в пробке
+            int jamCount = calculateJamCount(k, n, cars);
+            System.out.println(jamCount);
+        }
+
+        scanner.close();
+    }
+
+    private static int calculateJamCount(int k, int n, int[] cars) {
+        int jamCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            jamCount += cars[i]; // Добавляем количество машин, подъехавших за текущую минуту
+            jamCount -= k; // Учитываем количество машин, которые смогли повернуть
+            if (jamCount < 0) {
+                jamCount = 0; // Если машин в пробке меньше 0, устанавливаем 0
+            }
+        }
+
+        return jamCount;
+    }
+}
