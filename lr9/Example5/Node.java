73dae76dba17503169db079d06a7e75f193a5ea3
Sergey <minikinboost155w@mail.ru>
2025-03-03 13:35:10 +0500

    lr9

---
+++ lr9/Example5/Node.java
@@ -0,0 +1,10 @@
+package lr9.Example5;
+
+public class Node {
+    public int value;
+    public Node next;
+    Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+}
