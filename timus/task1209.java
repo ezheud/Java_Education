aaff5347195ffc14a8cafcfef3572c745c5706ad
ezheud <minikinboost155w@mail.ru>
2025-06-17 14:46:15 +0500

    more timus

--- timus/task1209.java
+++ timus/task1209.java
@@ -7,6 +7,13 @@ import java.io.InputStreamReader;
 
 public class task1209 {
 
+    //Исходные данные
+В первой строке находится целое число N (1 ≤ N ≤ 65535). 
+В i-й из N последующих строк записано целое число Ki — номер позиции в последовательности (1 ≤ Ki ≤ 231 − 1).
+Результат
+Выведите через пробел N цифр. i-я цифра должна равняться цифре, которая находится в описанной выше последовательности 
+на позиции с номером Ki.
+
     public static void main(String[] args) throws IOException {
         String inputFileName = "timus_task/input.txt";
         boolean oj = System.getProperty("ONLINE_JUDGE") != null;
